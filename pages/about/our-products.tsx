af9cb23d7322ba93ac969f9fc58a4b757ef7e995
winwoodj <winwoodj@coventry.ac.uk>
2021-09-15 17:46:21 +0100

    Add About Services page

--- pages/about/our-products.tsx
+++ pages/about/our-products.tsx
@@ -26,35 +26,35 @@ const OurProducts = () => {
         line1="At Groceries 4U we make getting your 5-a-day easy."
         line2="Enjoy great prices on a wide range of fresh fruit"
         line3="and vegetables."
-        image="/fruit_veg.jpg" alt="test">
+        image="/fruit_veg.jpg" alt="An image of an assortment of fruit and vegetables.">
         </RAboutDetail>
 
       <LAboutDetail header="Meat Counter" 
             line1="At our meat counter you'll find fresh meat and poultry," 
             line2="served to you with a smile by our in-house butchers." 
             line3="All our meat is sourced from British and Irish farmers." 
-            image="/meat.jpg" alt="test">
+            image="/meat.jpg" alt="An image of a meat counter with various cuts of meat.">
         </LAboutDetail>
 
       <RAboutDetail header="Dairy Range"
         line1="We source the best quality free-range dairy products"
         line2="and deliver them to you at competive prices. Enjoy"
         line3="our full range today."
-        image="/dairy.jpg" alt="test">
+        image="/dairy.jpg" alt="An image of various dairy products">
         </RAboutDetail>
 
         <LAboutDetail header="Freezer"
         line1="Our freezer range includes many of the UK's favourite"
         line2="brands alongside a host of our own frozen products and"
         line3="ready-meals."
-        image="/freezer.jpg" alt="test">
+        image="/freezer.jpg" alt="An image of the frozen aisle at a supermarket.">
         </LAboutDetail>
 
         <RAboutDetail header="Kitchen and Bathroom" 
             line1="At Groceries 4U we cover all your household needs" 
             line2="that's why in our kitchen and bathroom ranges you'll" 
             line3="find everything you need to keep them in top shape." 
-            image="/bathroom.jpg" alt="test">
+            image="/bathroom.jpg" alt="An image of someone carrying bathroom supplies.">
         </RAboutDetail>
 
       </main>
