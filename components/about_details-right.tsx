94fed069377c315c58820c784fb7dc7f4e0c3c3e
winwoodj <winwoodj@coventry.ac.uk>
2021-09-15 15:42:21 +0100

    Add About Products page and create components for it

---
+++ components/about_details-right.tsx
@@ -0,0 +1,19 @@
+import { IAboutDetail } from "../models/about-detail";
+import Image from 'next/image'
+
+const RAboutDetail:React.FC <IAboutDetail> = ({ header, line1, line2, line3, image, alt }) => {
+    return (
+        <div className="flex w-full bg-white mb-4 rounded-2xl bg-blue-500">
+            <div className="p-6 flex-grow text-center">
+                <h1 className="pb-5 text-5xl font-semibold font-sans">{header}</h1>
+                <h1>{line1}</h1>
+                <h1>{line2}</h1>
+                <h1>{line3}</h1>
+            </div>
+            <Image src={image} alt={alt} width="320" height="200"/>
+        </div>
+    )
+}
+
+export default RAboutDetail;
+
